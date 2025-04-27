64fcdb5c47349bb7e90e432f49747130cff75c01
Ktoto112 <19.12.2006sasha@gmail.com>
2025-04-27 17:11:09 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Bus {
+private:
+    string driverName;
+    int busNumber;
+    int routeNumber;
+    string brand;
+    int startYear;
+    int mileage;
+
+public:
+    
+    void setData(string dName, int bNumber, int rNumber, string bBrand, int sYear, int mMileage) {
+        driverName = dName;
+        busNumber = bNumber;
+        routeNumber = rNumber;
+        brand = bBrand;
+        startYear = sYear;
+        mileage = mMileage;
+    }
+
+   
+    void display() {
+        cout << "Driver: " << driverName << ", Bus №: " << busNumber
+            << ", Route №: " << routeNumber << ", Brand: " << brand
+            << ", Year of commencement of operation: " << startYear
+            << ", Run: " << mileage << " km" << endl;
+    }
+
+   
+    int getRouteNumber() {
+        return routeNumber;
+    }
+
+   
+    bool isMoreThan10Years(int currentYear) {
+        return (currentYear - startYear) > 10;
+    }
+
+    
+    bool hasBigMileage() {
+        return mileage > 10000;
+    }
+};
+
+int main() {
+    const int SIZE = 3; 
+    Bus buses[SIZE];
+
+    
+    buses[0].setData("Ivanenko I.I.", 101, 12, "Mercedes", 2010, 12000);
+    buses[1].setData("Petrenko P.P.", 102, 15, "Volvo", 2018, 8000);
+    buses[2].setData("Sydorenko S.S.", 103, 12, "MAN", 2005, 20000);
+
+    int searchRoute;
+    cout << "Enter the route number to search: ";
+    cin >> searchRoute;
+
+    int currentYear = 2025; 
+
+    cout << "\na) Buses on the route №" << searchRoute << ":\n";
+    for (int i = 0; i < SIZE; i++) {
+        if (buses[i].getRouteNumber() == searchRoute) {
+            buses[i].display();
+        }
+    }
+
+    cout << "\nb) Buses in operation for more than 10 years:\n";
+    for (int i = 0; i < SIZE; i++) {
+        if (buses[i].isMoreThan10Years(currentYear)) {
+            buses[i].display();
+        }
+    }
+
+    cout << "\nc) Buses with mileage over 10,000 km:\n";
+    for (int i = 0; i < SIZE; i++) {
+        if (buses[i].hasBigMileage()) {
+            buses[i].display();
+        }
+    }
+
+    return 0;
+}
